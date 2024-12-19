a73ee805db35efcc2d143c4e0bc0aaa610627cfb
BunnY <w@bunnywong.com>
2024-12-20 01:47:01 +0800

    TRY to cache sound files

--- sw.js
+++ sw.js
@@ -4,29 +4,71 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
-  // Add your sound files here
-  '/sounds/sound_haha.mp3',
-  '/sounds/sound_my-mom.mp3',
-  '/sounds/sound_hit-ear.mp3',
-  '/sounds/sound_fart.mp3',
-  '/sounds/sound_don-bell.mp3',
-  '/sounds/sound_bo-smile.mp3',
-  '/sounds/sound_whats-jp.mp3',
-  '/sounds/sound_applause-lite.mp3',
-  '/sounds/sound_applause-max.mp3',
-  '/sounds/sound_ball-game-hyper.mp3',
+  // Cache all your sound files
+  '/sounds/haha.mp3',
+  '/sounds/my-mom.mp3',
+  '/sounds/hit-ear.mp3',
+  '/sounds/fart.mp3',
+  '/sounds/don-bell.mp3',
+  '/sounds/bo-smile.mp3',
+  '/sounds/applause-lite.mp3',
+  '/sounds/applause-max.mp3',
 ]
 
+// Install event - cache all initial resources
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache')
+      return cache.addAll(urlsToCache)
+    })
   )
 })
 
+// Fetch event - serve from cache first, then network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response
+      }
+
+      // Clone the request because it's a one-time use stream
+      const fetchRequest = event.request.clone()
+
+      return fetch(fetchRequest).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response
+        }
+
+        // Clone the response because it's a one-time use stream
+        const responseToCache = response.clone()
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache)
+        })
+
+        return response
+      })
+    })
+  )
+})
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
   )
 })
