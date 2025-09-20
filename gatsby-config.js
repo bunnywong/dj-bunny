27382521961ae0dd76fe2036b257438885784567
BunnY <w@bunnywong.com>
2025-09-21 01:32:42 +0800

    FIX app icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `icons/dj-bunny.png`,
       },
     },
   ],
